675327d3f7e00ebc3c5d1d1c3ee0e38df80c5e5c
Nana Owusu Effah <noeffah@st.knust.edu.gh>
2023-09-22 23:13:24 +0000

    invasion here go

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 25, 230)
+        self.bg_color = (230, 230, 230)
 
         # Ship settings
         self.ship_speed = 1
@@ -18,3 +18,9 @@ class Settings:
         self.bullet_color = (60, 60, 64)
 
         self.bullets_allowed = 5
+
+        # Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 0.5
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
